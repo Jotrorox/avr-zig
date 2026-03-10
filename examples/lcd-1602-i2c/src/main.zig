bb4228338a7acb3279372841e40402d5072ff2da
jotrorox <mail@jotrorox.com>
2026-03-10 11:56:00 +0100

    Simplify and focus down examples

--- examples/lcd-1602-i2c/src/main.zig
+++ examples/lcd-1602-i2c/src/main.zig
@@ -1,6 +1,5 @@
 const avr = @import("avr_zig");
 const hd44780_i2c = avr.drivers.display.hd44780_i2c;
-const i2c = avr.i2c;
 const time = avr.time;
 const uart = avr.uart;
 
@@ -14,14 +13,12 @@ var display: Display16x2 = .{};
 
 pub fn main() void {
     uart.init(115200);
-    i2c.init();
-
-    scanI2cBus();
-    if (!initDisplay()) {
-        uart.write("LCD init failed at 0x27/0x3F\r\n");
-        while (true) {
-            time.sleep(1000);
-        }
+    
+    display.address = hd44780_i2c.default_address;
+    // If you have one with the 0x3F address just use .alternate_address
+    if (!display.init()) {
+        uart.write("LCD Not found!\n");
+        while (true) { time.sleep(500); }
     }
 
     var page: u8 = 0;
@@ -41,23 +38,6 @@ pub fn main() void {
     }
 }
 
-fn initDisplay() bool {
-    display.address = hd44780_i2c.default_address;
-    if (display.init()) {
-        uart.write("LCD found at 0x27\r\n");
-        return true;
-    }
-
-    display.address = hd44780_i2c.alternate_address;
-    if (display.init()) {
-        uart.write("LCD found at 0x3F\r\n");
-        return true;
-    }
-
-    display.address = hd44780_i2c.default_address;
-    return false;
-}
-
 fn renderPage(page: u8) void {
     @setRuntimeSafety(false);
 
@@ -72,28 +52,4 @@ fn renderPage(page: u8) void {
     }
 
     display.writeLine(1, row[0..]);
-}
-
-fn scanI2cBus() void {
-    uart.write("I2C scan:\r\n");
-    const count = i2c.scan(reportDevice);
-    if (count == 0) {
-        uart.write("  no devices found\r\n");
-    }
-}
-
-fn reportDevice(address: u7) void {
-    uart.write("  found device at 0x");
-    writeHexByte(@as(u8, address));
-    uart.write("\r\n");
-}
-
-fn writeHexByte(value: u8) void {
-    writeHexNibble(value >> 4);
-    writeHexNibble(value & 0x0F);
-}
-
-fn writeHexNibble(value: u8) void {
-    const digit = if (value < 10) '0' + value else 'A' + (value - 10);
-    uart.write_ch(digit);
 }
--- examples/ssd1306-demo/src/main.zig
+++ examples/ssd1306-demo/src/main.zig
@@ -1,31 +1,18 @@
 const avr = @import("avr_zig");
-const gpio = avr.hal.gpio;
-const i2c = avr.hal.i2c;
 const time = avr.hal.time;
 const uart = avr.hal.uart;
 const ssd1306 = avr.drivers.display.ssd1306;
 
 const Display128x64 = ssd1306.Display(128, 64);
 
-var current: u8 = '!';
 var display: Display128x64 = .{};
 
 pub fn main() void {
     uart.init(115200);
-    i2c.init();
-    gpio.init(.D13, .out);
 
-    scanI2cBus();
     initDisplay();
 
     while (true) {
-        uart.write_ch(current);
-        current = if (current < '~') current + 1 else '!';
-        if (current == '!') {
-            uart.write("\r\n");
-        }
-
-        gpio.toggle(.D13);
         time.sleep(500);
     }
 }
@@ -56,28 +43,4 @@ fn initDisplay() void {
     } else {
         uart.write("SSD1306 update failed\r\n");
     }
-}
-
-fn scanI2cBus() void {
-    uart.write("I2C scan:\r\n");
-    const count = i2c.scan(reportI2cDevice);
-    if (count == 0) {
-        uart.write("  no devices found\r\n");
-    }
-}
-
-fn reportI2cDevice(address: u7) void {
-    uart.write("  found device at 0x");
-    writeHexByte(@as(u8, address));
-    uart.write("\r\n");
-}
-
-fn writeHexByte(value: u8) void {
-    writeHexNibble(value >> 4);
-    writeHexNibble(value & 0x0F);
-}
-
-fn writeHexNibble(value: u8) void {
-    const digit = if (value < 10) '0' + value else 'A' + (value - 10);
-    uart.write_ch(digit);
 }
